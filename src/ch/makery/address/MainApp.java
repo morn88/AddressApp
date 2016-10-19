16c2477cdd59345f6c23847297a0ac6b8ae1bf55
m0rn <morn88@gmail.com>
2016-10-19 11:11:56 +0300

    урок 2

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -1,17 +1,44 @@
-package ch.makery.address;/**
+package ch.makery.address;
+/**
  * Created by m0rn on 19.10.2016.
  */
 
+import ch.makery.address.model.Person;
+import ch.makery.view.PersonOverviewController;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainApp extends Application {
     private Stage primaryStage;
     private BorderPane rootLayout;
+    //Данные в виде наблюдаемого списка адресов
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
 
-    public static void main(String[] args) {
-        launch(args);
+    //Конструктор
+    public MainApp(){
+        // В качестве образца добавляем некоторые данные
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+    //Возвращаем данные в виде наблюдаемого списка адресов.
+    public ObservableList<Person> getPersonData(){
+        return personData;
     }
 
     @Override
@@ -22,11 +49,50 @@ public class MainApp extends Application {
         showPersonalOverview();
     }
 
-    private void showPersonalOverview() {
-
-    }
     //Инициализируем корневой макет
     public void initRootLayout() {
-        
+        try {
+            //Загружаем корневой макет из fxml файла.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("../view/RootLayout.fxml"));
+            rootLayout = loader.load();
+
+            //Отображаем сцену, содержащую корневой макет.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Показывает в конревом макете сведения об адресатах
+    private void showPersonalOverview() {
+        try {
+            //Загружаем сведения об адресатах
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("../view/PersonOverview.fxml"));
+            AnchorPane personOverview = loader.load();
+
+            //Помещаем сведения об адресатах в центр корневого макета.
+            rootLayout.setCenter(personOverview);
+
+            //Даем контроллеру доступ к главному приложению.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Возвращаем главную сцену
+    public Stage getPrimaryStage(){
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
